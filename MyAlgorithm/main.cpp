80fc4cdad7783273d650ab69540ecf5b3268fa2b
deadwin <837115602@qq.com>
2019-06-20 23:58:19 +0800

    11

--- MyAlgorithm/main.cpp
+++ MyAlgorithm/main.cpp
@@ -54,13 +54,109 @@ void test278() {
 }
 
 
+void card_test() {
+
+}
+
+
+
+typedef struct NodeElem
+{
+	int data;
+	NodeElem * Next = NULL;
+}NodeElem;
+
+typedef NodeElem* ElemList;
+
+
+
+void insert(int data, ElemList list) {
+	ElemList p = list;
+	while (p->Next != NULL) {
+		if (p->Next->data > data) {
+			ElemList node = new NodeElem();
+			node->data = data;
+			node->Next = p->Next;
+			p->Next = node;
+			return;
+		}
+		else {
+			p = p->Next;
+		}
+	}
+	ElemList node = new NodeElem();
+	node->data = data;
+	node->Next = NULL;
+	p->Next = node;
+}
+void deleteNode(int data, ElemList list) {
+	ElemList p = list;
+	while (p->Next != NULL) {
+		if (p->Next->data == data) {
+			auto node = p->Next;
+			p->Next = p->Next->Next;
+			free(node);
+			return;
+		}
+		else {
+			p = p->Next;
+		}
+	}
+	cout << "can't find the data" << data << endl;
+}
+
+
 int main() {
 	cout << "hello algorithm" << endl;
-
+	cout << "a";
 	vector<int>vec = { 6,4,3,8,2,9,7 };
 
 	Solution s;
 	vec = { 0,0,0,1,1,3,3,3,4,4,5 };
 	s.removeDuplicates(vec);
+
+
+	ElemList e = new NodeElem();
+	
+	ElemList point =  e;
+	for (auto i = 0; i < 10; i++) {
+		ElemList n = new NodeElem();
+		n->data = i;
+		point->Next = n;
+		point = n;
+	}
+
+	point = e;
+	do {
+		point = point->Next;
+		cout << point->data << endl;
+	} while (point->Next != NULL);
+
+	insert(-2, e);
+
+	deleteNode(4, e);
+	point = e;
+	do {
+		point = point->Next;
+		cout << point->data << endl;
+	} while (point->Next != NULL);
+
+
+	ElemList p = (ElemList)malloc(sizeof(NodeElem));
+	ElemList k = (ElemList)malloc(sizeof(NodeElem));
+
+
+	for (auto i = 10; i < 15; i++) {
+		ElemList node = new NodeElem();
+		node->data = i;
+		node->Next = point->Next;
+		point->Next = node;
+	}
+	cout << endl;
+	point = e;
+	do {
+		point = point->Next;
+		cout << point->data << endl;
+	} while (point->Next != NULL);
 	return 0;
 }
